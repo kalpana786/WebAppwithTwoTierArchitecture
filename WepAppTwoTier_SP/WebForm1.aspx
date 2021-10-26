8bc276142b62f60aecfd3f0d0c199abb241394e4
Kalpana <52391133+kalpana786@users.noreply.github.com>
2021-10-26 16:20:06 +0530

    Add files via upload

---
+++ WepAppTwoTier_SP/WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WepAppTwoTier_SP.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>ID</td>
+                    <td><asp:TextBox ID="id" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Name</td>
+                    <td><asp:TextBox ID="Name" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Salary</td>
+                    <td><asp:TextBox ID="Salary" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td><asp:Button ID="Save" runat="server" Text="Save" OnClick="Save_Click" /></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WepAppTwoTier_SP/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WepAppTwoTier_SP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Save_Click(object sender, EventArgs e)
+        {
+            try {
+                string ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+                // string cmdstr = "INSERT INTO Employee(Eno, Ename, Salary) VALUES("+id.Text+",'"+Name.Text+"',"+Salary.Text+")";
+
+                SqlConnection Connection = new SqlConnection(ConnectionString);
+                //SqlCommand cmd = new SqlCommand(cmdstr, Connection); 
+                //cmd.CommandType = CommandType.Text;
+                //SqlCommand cmd = new SqlCommand(cmdstr, Connection);
+
+                SqlCommand cmd = new SqlCommand("Procedure_insertEmployee", Connection); // CALL STORED PROCEDURE
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", id.Text);
+                cmd.Parameters.AddWithValue("@ename", Name.Text);
+                cmd.Parameters.AddWithValue("@salary", Salary.Text);
+
+                Connection.Open();
+                if (cmd.ExecuteNonQuery() > 0)
+                    //  Response.Write("<script LANGUAGE='JavaScript'>alert('Data Saved')</script>");
+                    Response.Write("<script>alert('login successful');</script>");
+                else
+                    Response.Write("<script>alert('Not success');</script>");
+
+                Connection.Close();
+            }
+            catch (Exception obj) {
+                Response.Write($"<script>alert('{obj.Message} ');</script>");
+            }
+            finally {
+
+            }
+
+        }
+    }
+}
